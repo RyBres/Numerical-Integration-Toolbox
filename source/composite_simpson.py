6b626ec5bfaad5570c3d8727bf227a2a93f392d7
RyBres <bresnahanr18@gmail.com>
2024-05-16 23:56:20 -0400

    Update composite_simpson.py
    Revised set of exceptions and added functionality for python lambda to be used as an input expression

--- source/composite_simpson.py
+++ source/composite_simpson.py
@@ -1,11 +1,11 @@
-from typing import Union
+from typing import Union, Callable
 from sympy import Expr
 import numpy as np
 from sympy.utilities.lambdify import lambdify
 import sympy
 
 
-def composite_simpson(expr: sympy.Expr, a: Union[int, float], b: Union[int, float], n: int) -> float:
+def composite_simpson(expr: Union[sympy.Expr, Callable[[float], float]], a: Union[int, float], b: Union[int, float], n: int) -> float:
     '''Composite Simpson's integral approximation.
         
         Parameters:
@@ -18,26 +18,30 @@ def composite_simpson(expr: sympy.Expr, a: Union[int, float], b: Union[int, floa
             I (float): Floating point approximation of the integral
             
     '''
-    class InvalidnException(Exception):
-        "Raised when n is an odd number or non-integer."
-        pass
     
     class InvalidIntervalException(Exception):
         "Raised when the upper limit is less than the lower limit."
         pass
     
     if n % 2 != 0 or not isinstance(n, int):
-        raise InvalidnException("n must be an even integer.")
+        raise ValueError("'n' must be an even integer.")
         
     if a > b:
-        raise InvalidIntervalException("The upper limit 'a' must be greater than the lower limit 'b'.")
+        raise InvalidIntervalException("The upper limit 'b' must be greater than the lower limit 'a'.")
     
+    # Ensure the expression is evaluated as lambda
+    if isinstance(expr, sympy.Expr):
+        f = lambdify(sympy.symbols('x'), expr, modules=['numpy'])
+    elif callable(expr):
+        f = expr
+    else:
+        raise TypeError("'expr' must be either a sympy or lambda expression.")
+        
     # Get step size
     h = (b - a) / n
     
     # Define array of x values and compute yi at xi
     xarr = np.linspace(a, b, n+1)
-    f = lambdify(sympy.symbols('x'), expr, modules=['numpy'])
     yarr = f(xarr)
     
     # Get sum of odd and even indexed yi's
