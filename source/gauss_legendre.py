dee30b55e142d59a2712680ffad8abe0d132a203
RyBres <bresnahanr18@gmail.com>
2024-05-24 17:54:51 -0400

    First working gauss_legendre func
    I intend on vectorizing the loop for performance

--- source/gauss_legendre.py
+++ source/gauss_legendre.py
@@ -6,21 +6,44 @@ import sympy
 from numpy.polynomial import Polynomial, Legendre
 
 
+def gauss_legendre(f: sympy.Expr | Callable[[float], float], a: int | float, b: int | float, n: int) -> float:
+    """Gauss-Legendre integral approximation.
 
-def legendre(f: sympy.Expr | Callable[[float], float], a: int | float, b: int | float, n: int):
+        Parameters:
+            f (sympy.Expr | Callable[[float], float]): A SymPy expression or lambda expression
+            a (int | float): The lower limit of integration
+            b (int | float): The upper limit of integration
+            n (int): The number of iterations
+
+        Returns:
+            I (float): Floating point approximation of the integral
+
+        Author:
+            Ryan Bresnahan
+    """
+
+    class InvalidIntervalException(Exception):
+        """Raised when the upper limit is less than the lower limit."""
+        pass
+
+    if a > b:
+        raise InvalidIntervalException("The upper limit 'b' must be greater than the lower limit 'a'.")
+
+    # Ensure the expression is evaluated as lambda
+    if isinstance(f, sympy.Expr):
+        f = lambdify(sympy.symbols('x'), f, modules=['numpy'])
 
     # Find roots/weights w/ np. Could add in an approx routine with Newton's method on Chebyshev nodes
     nodes, weights = np.polynomial.legendre.leggauss(n)
 
-    h = (a - b) / 2 # outer constant of integral
+    px_arr = []
 
     for i in range(n):
-        # t is the root for that index
-        x = (0.5 * ((b - a)*nodes[i] + (b + a))**2) # is ~x centered on -1 to 1
-        # substitute ~x into f(x)
-        prod = weight * f(x)
-        # add to array showing elements of summation
+        x = 0.5 * ((b - a) * nodes[i] + (b + a))  # is ~x centered on -1 to 1
+        px = weights[i] * f(x)**2
+        px_arr.append(px)
 
-    # sum array times outer product (which is h)
+    # Sum the elements of array times the jacobian factor, producing answer
+    I = sum(px_arr) * (a - b) * 0.5
 
-
+    return I
