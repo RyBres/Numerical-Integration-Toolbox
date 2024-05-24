44b1c555c40bb3c740211ad156d170ebd6472926
RyBres <bresnahanr18@gmail.com>
2024-05-23 23:51:57 -0400

    Folded and used numpy instead of Newton/Chebyshev approx of roots (thatll  come later maybe)

--- source/gauss_legendre.py
+++ source/gauss_legendre.py
@@ -10,13 +10,13 @@ from numpy.polynomial import Polynomial, Legendre
 def legendre(f: sympy.Expr | Callable[[float], float], a: int | float, b: int | float, n: int):
 
     # Find roots/weights w/ np. Could add in an approx routine with Newton's method on Chebyshev nodes
-    
+    nodes, weights = np.polynomial.legendre.leggauss(n)
 
     h = (a - b) / 2 # outer constant of integral
 
     for i in range(n):
         # t is the root for that index
-        x = (0.5 * ((b - a)*t + (b + a))**2) * h # is ~x
+        x = (0.5 * ((b - a)*nodes[i] + (b + a))**2) # is ~x centered on -1 to 1
         # substitute ~x into f(x)
         prod = weight * f(x)
         # add to array showing elements of summation
